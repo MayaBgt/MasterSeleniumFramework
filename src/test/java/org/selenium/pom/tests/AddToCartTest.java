fcb78c67000e45110c895c9df6fb613541e32b75
MayaBgt <maya.bogatskaya@gmail.com>
2024-06-04 16:14:13 -0400

    @Link added

--- src/test/java/org/selenium/pom/tests/AddToCartTest.java
+++ src/test/java/org/selenium/pom/tests/AddToCartTest.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 public class AddToCartTest extends BaseTest {
 
 
+    @Link("https://askomdch.com/")
     @Link(name = "allure", type = "Link")
     @TmsLink("165537")
     @Issue("165537")
